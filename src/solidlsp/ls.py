bc047a6d1ea939f6465dbecf8cd8c0b782c56c1c
pXel4 <alexpestroiu@gmail.com>
2026-09-14 23:27:16 +0300

    Fix buffer ownership when nested open fails

--- src/solidlsp/ls.py
+++ src/solidlsp/ls.py
@@ -1311,9 +1311,9 @@ class SolidLanguageServer(ABC):
             assert fb.uri == uri
             assert fb.ref_count >= 1
 
-            fb.ref_count += 1
             if open_in_ls:
                 fb.ensure_open_in_ls()
+            fb.ref_count += 1
         else:
             version = 0
             language_id = self._get_language_id_for_file(relative_file_path)
